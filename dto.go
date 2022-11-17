06cf2edc8c905fd5934a31fdffd3ec1892e37a60
Lync <golangaddict@gmail.com>
2022-11-17 16:14:59 -0500

    upd

--- dto.go
+++ dto.go
@@ -1,19 +1,19 @@
 package bitpay
 
 type CreateInvoiceParams struct {
-	Description     string  `json:"itemDesc"`
-	Price           float64 `json:"price"`
-	Currency        string  `json:"currency"`
-	OrderId         string  `json:"orderId"`
-	NotificationURL string  `json:"notificationURL"`
-	RedirectURL     string  `json:"redirectURL"`
-	Buyer           Buyer   `json:"buyer"`
+	Description         string  `json:"itemDesc"`
+	Price               float64 `json:"price"`
+	Currency            string  `json:"currency"`
+	OrderId             string  `json:"orderId"`
+	NotificationURL     string  `json:"notificationURL"`
+	RedirectURL         string  `json:"redirectURL"`
+	ExtendNotifications bool    `json:"extendedNotifications"`
+	Buyer               Buyer   `json:"buyer"`
 }
 
 type CreateInvoice struct {
 	Data struct {
 		Invoice
-		ExtendNotifications bool `json:"extendedNotifications"`
 	} `json:"data"`
 }
 
